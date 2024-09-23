5b6797a8a003107466b0480188d80f25158e391d
Dananjaya24737 <prasannamadusannka804@gmail.com>
2024-09-23 13:30:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://prasannamadusannka804:0AhDNFB3NQ371mZI@cluster0.qkdgy.mongodb.net/', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
